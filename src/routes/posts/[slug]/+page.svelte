1aa7870c0c9bdfe882c0217534d184fc8fec363f
ccozens <ccozens@gmail.com>
2024-03-05 10:40:09 +0000

    installed @sveltejs/vite-plugin-svelte after run check showed it wasn't found

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import type { SvelteComponentTyped } from 'svelte/internal';
+	import type { SvelteComponent } from 'svelte';
 
 	import PageHead from '$lib/components/PageHead.svelte';
 	import ArticleTitle from '$lib/components/ArticleTitle.svelte';
@@ -8,7 +8,7 @@
 
 	export let data: PageData;
 
-	type C = $$Generic<typeof SvelteComponentTyped<any, any, any>>;
+	type C = $$Generic<typeof SvelteComponent<any, any, any>>;
 	$: component = data.component as unknown as C;
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,7 @@ const config = {
 
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+	preprocess: [mdsvex(mdsvexConfig), vitePreprocess({})],
 
 	kit: {
 		adapter: adapter({ strict: false })
